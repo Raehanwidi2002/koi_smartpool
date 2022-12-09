dcc62c49dba044975c88a9d8524b5e96b92adb96
Raehanwidi2002 <rehanwidisugiarto2002@gmail.com>
2022-12-09 11:09:43 +0700

    Firebase

--- lib/app/modules/signup/controllers/signup_controller.dart
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
-    FirebaseAuth auth = FirebaseAuth.instance;
+class SignupController extends GetxController {
+   FirebaseAuth auth = FirebaseAuth.instance;
 
   Stream<User?> streamAuthStatus(){
     return auth.authStateChanges();
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'firebase_options.dart';
+import 'package:koi_smartpool/app/controllers/auth_controller.dart';
+import 'package:koi_smartpool/app/utils/loading.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+    MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  final authC = Get.put(AuthController(), permanent: true);
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: authC.streamAuthStatus(),
+      builder: (context, snapshot) {
+        print(snapshot);
+        if (snapshot.connectionState == ConnectionState.active){
+        return GetMaterialApp(
+        title: "Aplication",
+        initialRoute: Routes.HOME,
+        getPages: AppPages.routes,
+      );
+        }
+      return LoadingView();
+    }
+    );
+  }
 }
